986def0e1638d58947957b74f280e451a42526b1
anloffredo <loffredo.ap@gmail.com>
2019-10-02 00:08:31 -0700

    framework update changes
    framework update changes

---
+++ Attributes/ServiceProviderAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.DependencyInjection.Abstractions.Attributes
+{
+    /// <summary>
+    /// Allows attributing a class so that the DI automatically registers it to itself with the given Service Provider type
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ServiceProviderAttribute : Attribute
+    {
+        /// <summary>
+        /// The type of the service provider that should be used to resolve this class
+        /// </summary>
+        public Type ServiceProvider { get; set; }
+
+        /// <summary>
+        /// The types that should resolve to this class
+        /// </summary>
+        public Type[] RegisteredTypes { get; set; }
+
+        /// <summary>
+        /// Constructs a new instance of this attribute
+        /// </summary>
+        /// <param name="serviceProvider">The type of the service provider that should be used to resolve this class</param>
+        /// <param name="registeredTypes">The types that should resolve to this class</param>
+        public ServiceProviderAttribute(Type serviceProvider, params Type[] registeredTypes)
+        {
+            ServiceProvider = serviceProvider;
+            RegisteredTypes = registeredTypes;
+        }
+    }
+}
+
---
+++ Interfaces/IRegisterDependencies.cs
@@ -0,0 +1,13 @@
+﻿namespace Penguin.DependencyInjection.Abstractions.Interfaces
+{
+    /// <summary>
+    /// Denotes that this class should be used by the Penguin DI Engine to register dependencies on initialization
+    /// </summary>
+    public interface IRegisterDependencies
+    {
+        /// <summary>
+        /// A method that actually calls the static instance of the Penguin DI engine to register the instances
+        /// </summary>
+        void RegisterDependencies();
+    }
+}
