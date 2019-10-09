d24dc15ee7dab20639ddd2c890005d22ce868ba4
anloffredo <loffredo.ap@gmail.com>
2019-10-08 18:12:16 -0700

    framework updates
    framework updates

--- Interfaces/IConsolidateDependencies.cs
+++ Interfaces/IConsolidateDependencies.cs
@@ -8,23 +8,14 @@ namespace Penguin.DependencyInjection.Abstractions.Interfaces
     /// Marks a class as being capable of squashing multiple registered instances into a single instance of the registered type, intended for ListProviders that have their own internal logic about prioritizing dependency usage
     /// </summary>
     /// <typeparam name="TDependency">The type of provider that this class consolidates</typeparam>
-    /// <typeparam name="TInstantiation">The type of provider that this class consolidates</typeparam>
-    public interface IConsolidateDependencies<TDependency, TInstantiation> where TInstantiation : TDependency
+    public interface IConsolidateDependencies<TDependency>
     {
-        /// <summary>
-        /// Construct a new instance of the consolidating class. This is called before the children are 
-        /// instantiated in the event that one of the childred indirectly requires a TDependency, so that an instance 
-        /// can exist before resolution is called so that we dont have a 
-        /// </summary>
-        /// <returns></returns>
-        TInstantiation Construct();
 
         /// <summary>
-        /// Squashes a list of registered instances down to a single consolidated instance
+        /// Squash the list down
         /// </summary>
-        /// <param name="instantiation">The previously constructed instance of this consolidator, ready to be filled</param>
-        /// <param name="Instances">The IEnumerable of existing instances returned by the DI</param>
-        /// <returns>A single squashed instance that uses all of the existing DI instances</returns>
-        void Populate(TInstantiation instantiation, IEnumerable<TDependency> Instances);
+        TDependency Consolidate(IEnumerable<TDependency> dependencies);
+
+
     }
 }
