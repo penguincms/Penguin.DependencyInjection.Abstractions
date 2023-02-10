d4f3987e26c12f2a747b1855bf962e2a5c347b58
Anthony Loffredo <loffredo.ap@gmail.com>
2023-02-10 01:38:10 -0800

    Autocommit
    Autocommit

--- Attributes/DependencyRegistrationAttribute.cs
+++ Attributes/DependencyRegistrationAttribute.cs
@@ -17,7 +17,7 @@ namespace Penguin.DependencyInjection.Abstractions.Attributes
         /// Constructs a new instance using the provided lifetime definition
         /// </summary>
         /// <param name="lifetime">The lifetime of the instantiated dependency</param>
-        public DependencyRegistrationAttribute(ServiceLifetime lifetime)
+        protected DependencyRegistrationAttribute(ServiceLifetime lifetime)
         {
             this.Lifetime = lifetime;
         }
